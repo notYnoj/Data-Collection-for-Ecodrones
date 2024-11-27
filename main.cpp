a9ac954a5b94c909c05857830548492041032291
Jonathan <jjalvarado14@cps.edu>
2024-11-27 17:05:14 -0600

    setting up github

---
+++ main.cpp
@@ -0,0 +1,6 @@
+/*
+The main goal of this repository is to simplify the process of data collection
+We're going to start with opening mp4 files in C++
+-> We might sidetrack into files as drone videos may not be in mp4 (We'll make a file convertor)
+
+*/
